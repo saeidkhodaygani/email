577bb388d14f70e5e7e6159282049df30c98a5d1
aminsalami <aminsalami@pm.me>
2020-07-22 23:15:30 +0430

    Add /me endpoint

--- src/django_web_app/router.py
+++ src/django_web_app/router.py
@@ -1,7 +1,7 @@
 from users.api.viewsets import userviewsets
 from users.api.views import MeApiHandler
 from rest_framework import routers 
-  
-router = routers.DefaultRouter() 
-router.register('user', userviewsets, basename ='user_api') 
-#router.register('user', MeApiHandler.as_view(), basename ='user_api') 
+
+router = routers.DefaultRouter()
+router.register('user', userviewsets, basename ='user_api')
+#router.register('user', MeApiHandler.as_view(), basename ='user_api')
--- src/users/api/serializers.py
+++ src/users/api/serializers.py
@@ -1,9 +1,23 @@
-from rest_framework import serializers 
-from django.contrib.auth.models import User 
-from blog.models import UserProfile  
+from rest_framework import serializers
+from blog.models import UserProfile
 
-class userSerializers(serializers.ModelSerializer): 
-  
-    class Meta: 
+
+class ContactSerializer(serializers.ModelSerializer):
+    """
+    """
+    class Meta:
         model = UserProfile
-        fields =  '__all__'#['user']
+        fields = ['id', 'role']
+
+class UserSerializers(serializers.ModelSerializer):
+    """
+    """
+    class Meta:
+        model = UserProfile
+        fields =  ['pk', 'role', 'contact']
+
+    contact = serializers.SerializerMethodField()
+
+    def get_contact(self, obj):
+        # We cant use UserSerializer because of recursive serialization on `contact` field
+        return ContactSerializer(obj.contact.all(), many=True).data
